8156455ebf70b25dd1a684d531d09e27fe902ce9
azzararh <azzahraramadhani7@gmail.com>
2025-10-21 15:44:37 +0700

    Create CreditBy.java

---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/CreditBy.java
@@ -0,0 +1,9 @@
+package util;
+
+public class CreditBy {
+    public static void print(String nim, String nama) {
+        System.out.println("\ncredit by: Azzahra Ramadhani " + nim + " 240320561 " + nama);
+    }
+}
+
+
